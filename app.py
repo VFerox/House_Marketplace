6d6bdbea233239fbaaea559e6c9c16c074344a5c
Ricky <rrautiainen13@gmail.com>
2025-09-20 15:41:32 +0300

    Update application

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def index():
     return "hei"
 
+@app.route("/Page1")
+def page1():
+    return "First Page"
+
+@app.route("/Page2")
+def page2():
+    return "Second Page"
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
